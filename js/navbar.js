af73f3ee8db85d61a0e17330f2c23a2b21a12438
GitSkynet <carlos.curtido@gmail.com>
2020-10-09 16:31:01 +0200

    OpenBooksV5

--- js/navbar.js
+++ js/navbar.js
@@ -1,9 +1,12 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
     var x = document.getElementById("myTopnav");
+    console.log(x)
     if (x.className === "topnav") {
         x.className += " responsive";
     } else {
         x.className = "topnav";
     }
+    console.log('funcion')
 }
+console.log('carga script')
