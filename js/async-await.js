8f88c14002de01205cf05251b88adafdce88e80a
GitSkynet <carlos.curtido@gmail.com>
2020-10-11 15:06:14 +0200

    Header, section y main page

--- js/async-await.js
+++ js/async-await.js
@@ -1,4 +1,4 @@
-const booksUrl = 'category=libros_programacion&criteria=most_viewed';
+const booksUrl = 'category=desarrollo_web&num_items=10';
 const wikiUrl = 'https://www.etnassoft.com/api/v1/get/?';
 const booksList = document.getElementById('apilibro');
 const btn = document.querySelector('button');
@@ -17,13 +17,31 @@ function generateHTML(data) {
     data.map(user => {
         const section = document.createElement('div');
         booksList.appendChild(section);
-        const thumbnail = user.thumbnail ? `<img src='${user.thumbnail}'>` : '';
+        const thumbnail = user.cover ? `<img src='${user.cover}'>` : '';
         section.innerHTML = `
-        ${thumbnail}
-        <span>${user.title}</span>
-        <h2>${user.author}</h2>
-        <p>${user.content_short}</p>
-        <a href="${user.url_download}">Descarga</a>
+        <div class="blog-card alted">
+            <div class="metadate">
+                <div class="photo2">${thumbnail}</div>
+                <ul class="details">
+                    <li class="author"><a href="#">Jane Doe</a></li>
+                    <li class="date">July. 15, 2015</li>
+                    <li class="tags">
+                        <ul>
+                            <li><a href="#">Learn</a></li>
+                            <li><a href="#">Code</a></li>
+                            <li><a href="#">JavaScript</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+            <div class="description">
+                <h1 class="targetheader1">${user.title}</h1>
+                <h2 class="targetheader2">${user.author}</h2>
+                <p class="targetp">${user.content_short}</p>
+                <p class="read-more">
+                    <a href="#">Read More</a>
+                </p>
+            </div>
         `
     })
 }
