9735880e281d014aaa9d7aebb5c434d75e0b9b59
GitSkynet <carlos.curtido@gmail.com>
2020-10-14 18:34:58 +0200

    Up Button add

--- js/simplescrollup.js
+++ js/simplescrollup.js
@@ -1,13 +1,13 @@
 $(document).ready(function(){ 
     $(window).scroll(function(){ 
-        if ($(this).scrollTop() > 100) { 
+        if ($(this).scrollTop() > 700) { 
             $('#scroll').fadeIn(); 
         } else { 
             $('#scroll').fadeOut(); 
         } 
     }); 
     $('#scroll').click(function(){ 
-        $("html, body").animate({ scrollTop: 0 }, 600); 
+        $("html, body").animate({ scrollTop: 0 }, 800); 
         return false; 
     }); 
 });
